319cff702311955d8b8174af8cd4f0e91f032d21
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-07 01:23:44 +0000

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +46,10 @@ public class DepartmentListController implements Initializable{
 	
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {   // ActionEvent event = argument to identify the controller
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage); // form to create
+	
 	}
 	
 	public void setDepartmentService(DepartmentService service) {  // connect DepartmentService with DepartmentController -> solid principle = inversion of control   
@@ -70,5 +81,27 @@ public class DepartmentListController implements Initializable{
 		
 	}
 	
+	private void createDialogForm(String absoluteName, Stage parentStage) {   // to instantiate the dialog window - goal = create a new department
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));  //load view
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();   // one stage in front of the other, so it is necessary to instantiate another stage
+			dialogStage.setTitle("Enter Department data");  // window title
+			dialogStage.setScene(new Scene(pane)); // What will the scene be? The loaded panel
+			dialogStage.setResizable(false);  // Can it be resizable or not?
+			dialogStage.initOwner(parentStage); 
+			dialogStage.initModality(Modality.WINDOW_MODAL);  // until you close the current window, you cannot access other windows
+			dialogStage.showAndWait();
+			
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+		}
+		
+		
+	}
 
 }
